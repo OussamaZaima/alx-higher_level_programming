7e903f9bb811a88e47ee3ebcd3a77b4bf6e32adb
oussama zaima <oussama.zaima2002@gmail.com>
2023-11-30 14:39:44 -0800

    task1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    ac = len(argv)
+    if ac == 1:
+        print("0 argumrnts,")
+    elif ac == 2
+    print("{:d} argument:".format(ac - 1))
+else:
+    print("{:d} argumrnts:".format(ac - 1))
+for i in range(1, ac):
+    print("{}: {}".format(i, argv[i]))
