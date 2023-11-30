5c610dc49715cbd18439d3e9c0434e286b89fe2a
oussama zaima <oussama.zaima2002@gmail.com>
2023-11-30 14:59:46 -0800

    task3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys
+    import sys
 
     count = len(sys.argv) - 1
     if count == 0:
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys
+    import sys
     sum = 0
     for i in range (len(sys.argv) - 1):
         sum += int(sys.argv[i + 1])
