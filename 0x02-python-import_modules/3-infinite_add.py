bab1b041e5952d3c7113fbaf5246c12b77369c16
oussama zaima <oussama.zaima2002@gmail.com>
2023-11-30 15:08:09 -0800

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
     sum = 0
-    for i in range(len(sys.argv) - 1):
-        sum += int(sys.argv[i + 1])
+    for arg in argv[1:]:
+        sum += int(arg)
         print(sum)
