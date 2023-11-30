56a3184e2a542855886fd62f5c01d0308af8fc51
oussama zaima <oussama.zaima2002@gmail.com>
2023-11-29 16:58:32 -0800

    fixtask10

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    c = i + ord('A')
-    if i % 2 == 1:
-        c+= 32
-        print("{:c}".format(c), end="")
+for ch in range(ord('z'), ord('a')-1, -1):
+    print('{:c}'.format(ch) if ch % 2 == 0 else chr(ch-32), end='')
