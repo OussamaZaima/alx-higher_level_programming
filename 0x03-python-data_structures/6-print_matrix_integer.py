217683c2692696d1bec051ab345776b2726d9019
oussama zaima <oussama.zaima2002@gmail.com>
2023-12-04 13:51:26 -0800

    task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix==[[]]):
+def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for idx, num in enumerate(row):
             if idx != 0:
