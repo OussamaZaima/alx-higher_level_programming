581a89483975ef016940248b3769664c542b687c
oussama zaima <oussama.zaima2002@gmail.com>
2023-12-04 14:26:39 -0800

    tasks 8 9 10 11

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     if my_list is None:
-        return
-    new_list = []
-    for n in my_list:
-        if n % 2 == 0:
-            new_list.append(True)
+        return None
+    listdivs = []
+    for i in my_list:
+        if (i % )2 == 0:
+            listdivs.append(True)
         else:
-            new_list.append(False)
-        return new_list
+            listdivs.append(False)
+        return listdivs
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None or len(my_list) == 0:
+    if len(my_list) < 1:
         return None
-    max_int = my_list[0]
-    for i in range(len(my_list)):
-        if max_int < my_list[i]:
-            max_int = my_last[i]
-            return max_int
+    list_copy = my_list.copy()
+    list_copy.sort()
+    return list_copy[-1]
