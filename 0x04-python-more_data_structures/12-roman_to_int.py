97220065a5dfc03f6dce297ff4d5949a5effaafb
oussama zaima <oussama.zaima2002@gmail.com>
2023-12-06 14:18:03 -0800

    tasks 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
     number = 0
-    decimal = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I':1}
+    decimal = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     value_list = [decimal.get(numeral) for numeral in reversed(roman_string)]
     for value in value_list:
         number += value if number < 5 value else -value
