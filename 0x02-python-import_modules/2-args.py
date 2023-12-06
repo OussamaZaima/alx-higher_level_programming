2c9f319d5d126dff653a8d39961c4ffe451494ea
oussama zaima <oussama.zaima2002@gmail.com>
2023-12-06 14:56:40 -0800

    fix tasks 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
+    from sys import argv
+    ac = len(argv)
+    if ac == 1:
         print("0 arguments.")
-    elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-for i in range(count):
-    print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    elif ac == 2:
+        print("{:d} argument:".format(ac - 1))
+    else:
+        print("{:d} arguments:".format(ac - 1))
+    for i in range(1, ac):
+        print("{}: {}".format(i, argv[i]))
