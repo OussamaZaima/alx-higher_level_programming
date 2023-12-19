91605a7af001bcd211abde55773ef36e3a8b32cf
oussama zaima <oussama.zaima2002@gmail.com>
2023-12-19 13:42:01 -0800

    tasks 0 1 2 3

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines a square class"""
+
+
+class Square:
+    """A simple square class. Empty for now"""
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines class Square with private attribute size
+and public attribute area.
+"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square.
+        Returns:
+            the area.
+        """
+        return (self.__size ** 2)
